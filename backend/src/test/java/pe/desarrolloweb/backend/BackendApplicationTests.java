19867bef077767a4e0da5318e0bedf6dae59e34c
ezerutp <u22320821@utp.edu.pe>
2025-08-26 21:35:04 -0500

    first commit

---
+++ backend/src/test/java/pe/desarrolloweb/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.desarrolloweb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
